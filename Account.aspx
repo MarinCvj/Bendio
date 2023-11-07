894c3ce269260a5c641c82fa262a8e1a65217982
MarinCvj <marin@cvjetkovic.me>
2023-11-07 22:36:43 +0100

    Creating background code for Account.aspx. Adding creating account and log in. Adding cookies to user

--- Account.aspx
+++ Account.aspx
@@ -1,5 +1,8 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Account.aspx.cs" Inherits="Bendio.Account" %>
 
+<%@ Register Src="~/Controls/Navigation_bar.ascx" TagPrefix="uc1" TagName="Navigation_bar" %>
+
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -13,10 +16,43 @@
 </head>
 <body>
     <form id="form1" runat="server">
+        <uc1:Navigation_bar runat="server" ID="Navigation_bar" />
         <div class="account-container">
             <h1>Create account or Log in</h1>
 
-            <h2></h2>
+            <div class="account">
+                <div class="create-acc">
+                    <h2>Create account</h2>
+                    <p>Email Address</p>
+                    <asp:TextBox runat="server" TextMode="Email" ID="email_create" placeholder="you@example.com"/>
+                    <p>Password</p>
+                    <asp:TextBox runat="server" TextMode="Password" ID="password_create" placeholder="Enter 8 character or more"/>
+
+                    <div class="remember-me">
+                        <input type="checkbox" />
+                        <p>Remember me</p>
+                    </div>
+
+                    <asp:Button CssClass="btn" runat="server" OnClick="Create_Account" Text="Create Account"/>
+                </div>
+            
+                <div class="log-in">
+                    <h2>Log in</h2>
+                    <p>Email Address</p>
+                    <asp:TextBox runat="server" TextMode="Email" id="email_log" placeholder="you@example.com"/>
+                    <p>Password</p>
+                    <asp:TextBox runat="server" TextMode="Password" ID="password_log" placeholder="Enter 8 character or more"/>
+
+                    <div class="remember-me">
+                        <asp:CheckBox runat="server" ID="remember_me"/>
+                        <p>Remember me</p>
+                    </div>
+
+                    <asp:Label runat="server" ID="wrong" Text="Wrong email or password." Visible="false"></asp:Label>
+
+                    <asp:Button CssClass="btn" runat="server" OnClick="Log_in" Text="Log in" />
+                </div>
+            </div>
         </div>
     </form>
 </body>
--- Account.aspx.cs
+++ Account.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,72 @@ namespace Bendio
 {
     public partial class Account : System.Web.UI.Page
     {
+        SqlConnection cnn;
         protected void Page_Load(object sender, EventArgs e)
         {
+            string cs = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+            cnn = new SqlConnection(cs);
+            cnn.Open();
+        }
 
+        private void CreateCookie(string email_address)
+        {
+            HttpCookie c = new HttpCookie("email");
+            c.Value = email_address;
+            c.Expires.Add(new TimeSpan(1, 0, 0));
+            Response.Cookies.Add(c);
+            Response.Redirect("~/Home.aspx");
+        }
+
+        protected void Create_Account(object sender, EventArgs e)
+        {
+            string email_text = email_create.Text;
+            string password_text = password_create.Text;
+
+            string user = "INSERT INTO [Bendio].[dbo].[User] (email, password) VALUES ('" + email_text + "','" + password_text + "')";
+            SqlCommand sqlCmd = new SqlCommand(user, cnn);
+            sqlCmd.ExecuteNonQuery();
+
+            if (remember_me.Checked)
+            {
+                CreateCookie(email_text);
+            }
+            else
+            {
+                Response.Redirect("~/Home.aspx");
+            }
+            cnn.Close();
+        }
+
+        protected void Log_in(object sender, EventArgs e)
+        {
+            string email_text = email_log.Text;
+            string password_text = password_log.Text;
+
+            string user_get = "SELECT * FROM [Bendio].[dbo].[User] WHERE email='" + email_text + "'" + "AND password='" + password_text + "'";
+            SqlCommand sqlCmd = new SqlCommand(user_get, cnn);
+
+            var user = 0;
+            using (SqlDataReader sqlReader = sqlCmd.ExecuteReader())
+            {
+                while (sqlReader.Read())
+                {
+                    user = 1;
+                    if (remember_me.Checked)
+                    {
+                        CreateCookie(email_text);
+                    }else
+                    {
+                        Response.Redirect("~/Home.aspx");
+                    }
+                }
+            }
+
+            if (user == 0)
+            {
+                wrong.Visible = true;
+            }
+            cnn.Close();
         }
     }
 }
